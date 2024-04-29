e715ef6285a8fba8fe371ef995d1652b0046f36e
FernandoGon19 <129014797+FernandoGon19@users.noreply.github.com>
2024-04-28 22:50:27 -0300

    adicionando primeiro commit

---
+++ src/Ex4/Modulo4.java
@@ -0,0 +1,9 @@
+package Ex4;
+
+public class Modulo4 {
+
+	public static void main(String[] args) {
+		System.out.print("Hello World EBAC");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Exercício_Módulo_4 {
+}
